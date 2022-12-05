533d7036b00bbfae9e778521c2cb55e569fb4a27
Maarten Smeyers <maartensmeyers@gmail.com>
2022-12-05 08:40:55 +0100

    Day 4 solution

---
+++ day04/part2.py
@@ -0,0 +1,20 @@
+with open("input.txt") as f:
+    entries = f.read().splitlines()
+
+cleanup = []
+for e in entries:
+    elves = e.split(',')
+    for el in elves:
+        x = el.split('-')
+        start = int(x[0])
+        end = int(x[1]) + 1
+        assignment = {x for x in range(start, end)}
+        cleanup.append(assignment)
+
+total = 0
+for i in range(0, len(cleanup) - 1, 2):
+    x = cleanup[i]
+    y = cleanup[i + 1]
+    if not x.isdisjoint(y):
+        total += 1
+print(total)
