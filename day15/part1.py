d57c4b6ab06005ee24929d36159e26e9ef9daa74
Maarten Smeyers <maartensmeyers@gmail.com>
2023-01-08 10:23:48 +0100

    Day 15 solution - part 1

--- day15/part1.py
+++ day15/part1.py
@@ -1,45 +1,49 @@
 def process_input():
-    with open("test.txt") as f:
+    with open("input.txt") as f:
         entries = f.read().splitlines()
-        coords = []
+        sensors = []
+        beacons = []
+        manhattan = []
         for e in entries:
             pos = e.split('=')
-            sx = int(pos[1][:pos[1].index(',')]) + 10
-            sy = int(pos[2][:pos[2].index(':')]) + 10
-            bx = int(pos[3][:pos[3].index(',')]) + 10
-            by = int(pos[4]) + 10
-            coords.append((sx, sy, bx, by))
-        max_value = max(max(i for i in j) for j in coords)
-        cave = [['.' for _ in range(2 * max_value + 1)] for _ in range(2 * max_value + 1)]
-        for c in coords:
-            sx, sy, bx, by = c
-            cave[sy][sx] = 'S'
-            cave[by][bx] = 'B'
-        return cave, coords
+            sx = int(pos[1][:pos[1].index(',')])
+            sy = int(pos[2][:pos[2].index(':')])
+            bx = int(pos[3][:pos[3].index(',')])
+            by = int(pos[4])
+            sensors.append((sx, sy))
+            beacons.append((bx, by))
+            distance_to_closest_beacon = abs(sx - bx) + abs(sy - by)
+            manhattan.append(distance_to_closest_beacon)
+        return sensors, beacons, manhattan
 
 
-def print_grid(g):
-    for i in g:
-        for j in i:
-            print(j, end='')
-        print()
-    print()
+def can_contain_beacon(sensors, beacons, manhattan, test):
+    if test in beacons:
+        return True
+    for i in range(len(sensors)):
+        sx, sy = sensors[i]
+        tx, ty = test
+        distance_to_closest_beacon = manhattan[i]
+        distance_to_test = abs(sx - tx) + abs(sy - ty)
+        if distance_to_test <= distance_to_closest_beacon:
+            return False
+    return True
 
 
 def solve():
-    cave, coords = process_input()
-    for line in coords:
-        sx, sy, bx, by = line
-        manhattan = abs(sx - bx) + abs(sy - by)
-        for y in range(sy - manhattan, sy + manhattan + 1):
-            for x in range(sx - manhattan + abs(sy - y), sx + manhattan + 1 - abs(sy - y)):
-                if cave[y][x] == '.':
-                    cave[y][x] = '#'
-    # print_grid(cave)
-    # for x in cave[20]:
-    #     print(x, end='')
-    # print()
-    print(sum([1 for x in cave[20] if x == '#']))
+    sensors, beacons, manhattan = process_input()
+    total = 0
+    target = 2000000
+    min_x = max_x = target
+    for i in range(len(sensors)):
+        sx, sy = sensors[i]
+        if sx - manhattan[i] < min_x:
+            min_x = sx - manhattan[i]
+        if sx + manhattan[i] > max_x:
+            max_x = sx + manhattan[i]
+    for i in range(min_x, max_x):
+        total += not can_contain_beacon(sensors, beacons, manhattan, (i, target))
+    print(total)
 
 
 if __name__ == "__main__":
